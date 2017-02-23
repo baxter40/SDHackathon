587c816b7bba42e62ea79b573f689d06cc376322
Hamish <hamish_baxter@hotmail.co.uk>
2017-02-23 17:44:08 +0000

    Back button work

--- SDApplication/app/src/main/java/com/example/matt/sdapplication/Basket.java
+++ SDApplication/app/src/main/java/com/example/matt/sdapplication/Basket.java
@@ -23,7 +23,7 @@ public class Basket extends AppCompatActivity implements NavigationView.OnNaviga
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);/*
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
